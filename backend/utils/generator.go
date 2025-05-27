045beb176fd3e0730d0cd1a137ba63aa70b2cfc9
nerfthisdev <353054@niuitmo.ru>
2025-05-27 23:07:54 +0300

    feat: add random shortened link generator

---
+++ backend/utils/generator.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const length = 6
+
+func GenerateCode() string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
